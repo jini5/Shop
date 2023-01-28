262e4d2faa29a71edd58ef140a929ecc93347c6a
jini5 <lim789789@gmail.com>
2023-01-29 01:12:03 +0900

    commit

---
+++ src/main/java/com/demo/Shop/Hello.java
@@ -0,0 +1,10 @@
+package com.demo.Shop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Hello {
+    private String data;
+}
---
+++ src/test/java/com/demo/Shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
