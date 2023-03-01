08a58dda063f561912232b073ee4b4d877e57201
jini5 <lim789789@gmail.com>
2023-03-01 13:09:28 +0900

    commit

--- src/main/java/com/demo/Shop/Hello.java
+++ src/main/java/com/demo/Shop/Hello.java
@@ -1,10 +1,10 @@
-package com.demo.Shop;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class Hello {
-    private String data;
-}
+//package com.demo.Shop;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//public class Hello {
+//    private String data;
+//}
